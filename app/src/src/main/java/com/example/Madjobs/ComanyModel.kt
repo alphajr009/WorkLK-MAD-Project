4f2f422c8d4eb03f6f1aaf5156714552af507ae8
Ravindu Rusith <122794096+RavinduRusith18@users.noreply.github.com>
2023-05-13 15:18:54 +0530

    Company UI Fixed and finalized project

---
+++ app/src/src/main/java/com/example/Madjobs/ComanyModel.kt
@@ -0,0 +1,14 @@
+package com.example.Madjobs
+
+data class ComanyModel(
+    var comId : String? = null,
+    var name : String? = null,
+    var country : String? = null,
+    var province : String? = null,
+    var distric : String? = null,
+    var address : String? = null,
+    var mobile : String? = null,
+    var type : String? = null,
+    var regNumber : String? = null,
+    var regDate : String? = null,
+)
